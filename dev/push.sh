eb958dbf6a66fb2c98b586fd001f2b7f2e42025e
OCEaster <oweneaster@outlook.com>
2022-04-01 01:24:09 +0100

    Auto Commit

--- dev/push.sh
+++ dev/push.sh
@@ -11,16 +11,17 @@ $DEV git checkout main
 
 # [CHECKOUT]External Mains
 cd Ext/Overlord && git branch --set-upstream-to=origin/main main && git checkout main
-cd ../Client && git branch --set-upstream-to=origin/main main && git checkout main
-cd ../..
+cd clients/default_angular && git branch --set-upstream-to=origin/main main && git checkout main
+cd ../default_react && git branch --set-upstream-to=origin/main main && git checkout main
+cd ~/Easter/Dev
 
 # [CHECKOUT]Internal Mains
 cd Int/Staging && git branch --set-upstream-to=origin/main main && git checkout main
-cd ../..
+cd ~/Easter/Dev
 
 # [CHECKOUT]System Mains
 cd Sys && git branch --set-upstream-to=origin/main main && git checkout main
-cd ..
+cd ~/Easter/Dev
 
 # [PULL] Repository & Recurse Submodules
 echo
