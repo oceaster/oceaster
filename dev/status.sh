24450c8e045b9be440e0a24ecd085f5924ffb5b1
OCEaster <oweneaster@outlook.com>
2022-04-01 06:37:20 +0100

    Auto Commit

--- dev/status.sh
+++ dev/status.sh
@@ -6,22 +6,30 @@ echo
 echo
 
 # STATUS Developer Environment Root
+echo
+echo "  ==: DEV :=="
 $DEV git status
+echo
+echo
 
 # STATUS External Mains
+echo "  ==: OVERLORD :=="
 cd Ext/Overlord && git status
 cd ../..
-
+echo
+echo
 # STATUS Internal Mains
+echo "  ==: STAGING :=="
 cd Int/Staging && git status
 cd ../..
-
+echo
+echo
 # STATUS System Mains
+echo "  ==: SYSTEM :=="
 cd Sys && git status
 cd ..
-
+echo
+echo
 # Done
-echo
-echo
 echo "  Done!"
 echo
