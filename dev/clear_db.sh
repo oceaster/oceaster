0862c458cf0ceb378a8f7c6c80bbefc18eb2ad1f
OCEaster <oweneaster@outlook.com>
2022-04-01 07:33:51 +0100

    added dev.clear_db

---
+++ dev/clear_db.sh
@@ -0,0 +1,13 @@
+echo
+echo "  [DEV]   CLEAR DB"
+echo
+echo "  [WARNING] Always use this script from"
+echo "    within your Dev.git root directory."
+echo
+echo
+echo "  Deleting Database..."
+echo
+echo
+cd ~/Easter/Dev/Ext/Overlord
+rm db.sqlite3
+echo
